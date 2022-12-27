9b0bb365156431feb45f8355259d306985652837
Volin13 <110535249+Volin13@users.noreply.github.com>
2022-12-27 23:30:42 +0200

    commit message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { ImageGellery, SearchBar } from '../components/index';
+import { Component } from 'react';
+import styled from 'styled-components';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Container = styled.div`
+  margin: 0 auto 20px;
+  width: 80%;
+  position: relative;
+`;
+export class App extends Component {
+  state = {
+    query: '',
+    showModal: false,
+  };
+
+  pictureQuery = query => {
+    this.setState({
+      query,
+      page: 1,
+    });
+  };
+  notify = message => toast.error(message);
+
+  render() {
+    const { query } = this.state;
+    return (
+      <Container>
+        <SearchBar pictureQuery={this.pictureQuery} />
+        <ImageGellery query={query} notify={this.notify} />
+        <ToastContainer position="top-right" autoClose={3000} theme="dark" />
+      </Container>
+    );
+  }
+}
