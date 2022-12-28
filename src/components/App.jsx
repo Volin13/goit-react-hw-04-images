db8262dd96fad9ce39af4f5815880ca765e6c187
Volin13 <110535249+Volin13@users.noreply.github.com>
2022-12-28 22:49:34 +0200

    commit message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,26 @@
 import { ImageGellery, SearchBar } from '../components/index';
-import { Component } from 'react';
 import styled from 'styled-components';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from 'react';
 
 const Container = styled.div`
   margin: 0 auto 20px;
   width: 80%;
   position: relative;
 `;
-export class App extends Component {
-  state = {
-    query: '',
-    showModal: false,
-  };
+export const notify = message => toast.error(message);
+export const App = () => {
+  const [query, setQuery] = useState('');
 
-  pictureQuery = query => {
-    this.setState({
-      query,
-      page: 1,
-    });
+  const pictureQuery = query => {
+    setQuery(query);
   };
-  notify = message => toast.error(message);
-
-  render() {
-    const { query } = this.state;
-    return (
-      <Container>
-        <SearchBar pictureQuery={this.pictureQuery} />
-        <ImageGellery query={query} notify={this.notify} />
-        <ToastContainer position="top-right" autoClose={3000} theme="dark" />
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <SearchBar pictureQuery={pictureQuery} />
+      <ImageGellery query={query} notify={notify} />
+      <ToastContainer position="top-right" autoClose={3000} theme="dark" />
+    </Container>
+  );
+};
